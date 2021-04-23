8c063bcfc60ac0e4367488e7a23b2e45b1ed8dd9
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-23 22:14:35 +0300

    add code js and html modify

--- Asma.js
+++ Asma.js
@@ -2,6 +2,7 @@
 
 
 let x= true; // true means X turn
+let sequares=[];
 const insert =(id)=>{
     let p= document.getElementById(id)
     if ( x && p.innerHTML=="" ){
@@ -14,7 +15,11 @@ const insert =(id)=>{
     }
 }
 const checkWinner =()=>{
-    for(let i =0;i<9;i++){
+    for(let i=0; i<9 ;i++){
      sequares[i]=document.getElementById('A'+i).innerHTML;
     }
 }
+if ( sequares[1]==sequares[2] && sequares[2]==sequares[3]&&sequares[1]!="" ){
+    alert ("player "+sequares[1]+" Wins the game ")
+}
+
