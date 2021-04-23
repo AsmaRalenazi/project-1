2a63ee0145a63907d53ad13a5541641a244c0a5f
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-23 20:24:37 +0300

    add code js ,and modify code html&css

---
+++ Asma.js
@@ -0,0 +1,14 @@
+
+let X= true; // true means X turn
+const insert =(id)=>{
+    let p= document.getElementById(sequare)
+    if (X){
+        sequare.innerHTML="X"
+        X=!X
+    }else{
+        document.getElementById(sequare)
+        sequare.innerHTML="O"
+        X=!X
+
+    }
+}
