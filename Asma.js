0f2e0b2cd128656371f90fcb09e20b8306ada306
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-23 20:39:50 +0300

    modify js

--- Asma.js
+++ Asma.js
@@ -1,14 +1,14 @@
 
-let X= true; // true means X turn
+let x= true; // true means X turn
 const insert =(id)=>{
-    let p= document.getElementById(sequare)
-    if (X){
-        sequare.innerHTML="X"
-        X=!X
-    }else{
-        document.getElementById(sequare)
-        sequare.innerHTML="O"
-        X=!X
+    let p= document.getElementById(id)
+    if ( x && p.innerHTML=="" ){
+        p.innerHTML="X"
+        x=!x
+    }else if (!x && p.innerHTML==""){
 
+        p.innerHTML="O"
+         x=!x
     }
+    return id;
 }
