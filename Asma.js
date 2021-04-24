b7559ead9d4be38ef4d52391af41005cfd293ec2
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-25 02:56:38 +0300

    add background ,edit code css,edit code js

--- Asma.js
+++ Asma.js
@@ -1,95 +1,4 @@
 
-// let result =[
-//  'x','x','x',
-//  'x','x','x',
-//  'x','x','x',
-// ]
-// //0  1  2
-// //3  4  5
-// //6  7  8
-// const random =Math.round(Math.random());
-// let starter= random === 0?'x':'o';
-// let p1v =document.getElementById('p1v')
-// p1v.innerText= random === 0?'x .':'o.';
-// let p2v =document.getElementById('p2v')
-// p2v.innerText= random === 0?'o':'x.';
-
-// const clickGame=()=>{
-//     if(document.getElementById(index).innerHTML){
-//         return;
-//     }
-//     document.getElementById(index).innerHTML=starter
-//     result[index]=starter
-//     starter=starter=== 'x'? 'o' :'x'
-   
-// }
-
-// chickResult=(str)=>{
-// return str.touppercase()==='xxx' ||str.touppercase() ==='ooo'
-// }
-
-// is3=(str)=>{
-//     return str.length===3;
-// }
-
-// winnerResult=()=>{
-// if ('xxx'=== str.touppercase()){
-// return (p1v.innerText === 'x')?'player1 (x)is Winner ':'player1 (x) is Winner '
-// }
-// return (p2v.innerText === 'x')?'player1 (o)is Winner ':'player1 (o) is Winner '
-// }
-
-// getResult=()=>{
-
-//     //horizontal
-// let row1=result[0]+result[1]+result[2];
-// let row2=result[3]+result[4]+result[5];
-// let row3=result[6]+result[7]+result[8];
-
-//   //vertical
-// let col1=result[0]+result[2]+result[3];
-// let col2=result[1]+result[4]+result[7];
-// let col3=result[2]+result[5]+result[8];
-
-//   //diagonal 
-// let sql1=result[0]+result[4]+result[8];
-// let sql2=result[2]+result[4]+result[6];
-
-
-//     if(chickResult(row1)){
-//         return'you win'
-//     }
-//     else if(chickResult(row2)){
-//         return'you win'
-//     } else if(chickResult(row3)){
-//         return'you win'
-//     }
-//     else if(chickResult(clo1)){
-//         return'you win'
-//     }
-//     else if(chickResult(clo2)){
-//         return'you win'
-//     }
-//     else if(chickResult(sql1)){
-//         return'you win'
-//     }
-//     else if(chickResult(sql2)){
-//         return'you win'
-//     }else (is3(row1)&&is3(row2) &&
-//     is3(row3)&&is3(col1)&&
-//     is3(col2)&&is3(col3)&&
-//     is3(sql1)&&is3(sql2)
-//     )
-//     return "No Winner"
-
-// }
-
-
-
-
-
-
-
 
 var turn= true; // true means X true 
 insert=(id)=>{
@@ -108,13 +17,10 @@ insert=(id)=>{
 
     
 reset=(A,B,C)=>{
-    document.getElementById('A'+A).style.color='#ff0000';
-    document.getElementById('A'+B).style.color='#ff0000';
-    document.getElementById('A'+C).style.color='#ff0000';  
+    document.getElementById('A'+A).style.color='#791616';
+    document.getElementById('A'+B).style.color='#791616';
+    document.getElementById('A'+C).style.color='#791616';  
 }
-
-// setTimeout =(function(){
-//     location.reload() },1000);  
 
 
 let sequares=[];
@@ -169,6 +75,3 @@ sequares[8]!==''&&sequares[9]!==''){
 
 
 }
-//❤
-//➶
-//⤲
