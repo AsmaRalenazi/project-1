1ac6c8e0dc4b3a077d4e0b433143370ab3f6e011
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-24 21:16:54 +0300

    edit code ja &html

--- Asma.js
+++ Asma.js
@@ -108,20 +108,18 @@ insert=(id)=>{
 
 
 reset=(A,B,C)=>{
-    document.getElementById('A'+A).style.color='#000';
-    document.getElementById('A'+B).style.color='#000';
-    document.getElementById('A'+C).style.color='#000';
+    document.getElementById('A'+A).style.color='#ff0000';
+    document.getElementById('A'+B).style.color='#ff0000';
+    document.getElementById('A'+C).style.color='#ff0000';  
 }
 
 setTimeout =(function(){
-location.reload();
-},2000);
-
+    location.reload() },2000);  
 
 
 let sequares=[];
 checkWinner =()=>{
-    for(let i=0; i<9 ;i++){
+    for(let i=1; i<10 ;i++){
      sequares[i]=document.getElementById('A'+i).innerHTML;
     }
 
@@ -129,7 +127,6 @@ checkWinner =()=>{
 if ( sequares[1]==sequares[2] && sequares[2]==sequares[3]&&sequares[1]!="" ){
     alert ("player "+sequares[1]+" Wins the game ツ")
     reset(1,2,3);
-    return
 }
 if ( sequares[4]==sequares[5] && sequares[5]==sequares[6]&&sequares[4]!="" ){
     alert ("player "+sequares[4]+" Wins the game ツ")
@@ -162,7 +159,12 @@ if ( sequares[3]==sequares[5] && sequares[5]==sequares[7]&&sequares[3]!="" ){
     alert ("player "+sequares[3]+" Wins the game ツ")
     reset(3,5,7);
 }
-return "No Winner"
+// if(is3(row1)&&is3(row2) &&
+//     is3(row3)&&is3(col1)&&
+//     is3(col2)&&is3(col3)&&
+//     is3(sql1)&&is3(sql2))
+alert ("No Winner")
+
 }
 //❤
 //➶
