b445659a58a443713b9255ac90b6d08e1a0129e9
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-04-26 01:52:55 +0300

    edit mobile design

--- Asma.js
+++ Asma.js
@@ -33,43 +33,50 @@ checkWinner =()=>{
 if ( sequares[1]==sequares[2] && sequares[2]==sequares[3]&&sequares[1]!="" ){
     alert ("player "+sequares[1]+" Wins the game ツ")
     reset(1,2,3);
+    return
 }
 if ( sequares[4]==sequares[5] && sequares[5]==sequares[6]&&sequares[4]!="" ){
     alert ("player "+sequares[4]+" Wins the game ツ")
-    reset(4,5,6);  
+    reset(4,5,6); 
+    return 
 }
 if ( sequares[7]==sequares[8] && sequares[8]==sequares[9]&&sequares[7]!="" ){
     alert ("player "+sequares[7]+" Wins the game ツ")
     reset(7,8,9);
+    return
 }
 //check vertical
 if ( sequares[1]==sequares[4] && sequares[4]==sequares[7]&&sequares[1]!="" ){
     alert ("player "+sequares[1]+" Wins the game ツ")
     reset(1,4,7);
+    return
 }
 if ( sequares[2]==sequares[5] && sequares[5]==sequares[8]&&sequares[2]!="" ){
     alert ("player "+sequares[2]+" Wins the game ツ")
     reset(2,5,8); 
+    return
 }
 if ( sequares[3]==sequares[6] && sequares[6]==sequares[9]&&sequares[3]!="" ){
     alert ("player "+sequares[3]+" Wins the game ツ")
     reset(3,6,9);
+    return
 }
 
 //check diagonal 
 if ( sequares[1]==sequares[5] && sequares[5]==sequares[9]&&sequares[1]!="" ){
     alert ("player "+sequares[1]+" Wins the game ツ")
     reset(1,5,9);
+    return
 }
 if ( sequares[3]==sequares[5] && sequares[5]==sequares[7]&&sequares[3]!="" ){
     alert ("player "+sequares[3]+" Wins the game ツ")
     reset(3,5,7);
+    return
 }
 if(sequares[1]!==''&&sequares[2]!==''&&sequares[3]!==''&&
 sequares[5]!==''&&sequares[6]!==''&&sequares[7]!==''&&
 sequares[8]!==''&&sequares[9]!==''){
-
-        alert ("No Winner")
+    return    alert ("No Winner")
     }
 
 
